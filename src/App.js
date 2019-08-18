24ca941c64f31dddab74135d6c0442f56f1ef579
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-18 12:11:54 +0200

    Render contacts list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts.js';
 
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  },
+  {
+    "id": "mohamed",
+    "name": "Mohamed Abdel Nasser",
+    "email": "mohdabdellnasser1@gmail.com",
+    "avatarURL": "http://localhost:5001/mohamed.jpg"
+  }
+
+]
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts}/>
       </div>
     )
   }
