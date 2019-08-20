1dcd2d5765ee40899abf19f159be09aa5c931f6f
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-21 00:12:48 +0200

    a bug fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowerRouter } from 'react-router-dom' 
+import { BrowserRouter } from 'react-router-dom' 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
-ReactDOM.render(<BrowerRouter><App /></BrowerRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
