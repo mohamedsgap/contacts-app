90107d25f1024660535d843d72b3016f0f6bf09a
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-21 01:25:12 +0200

    using Link component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
@@ -48,11 +49,11 @@ class ListContacts extends React.Component {
               value={this.state.query}
               onChange={(event) => this.updateQuery(event.target.value)}
             />
-            <a
-             href='#create'
+            <Link
+             to='/create'
              onClick={this.props.onNavigate}
              className='add-contact' 
-            >Add Contact</a>
+            >Add Contact</Link>
 
 
 
