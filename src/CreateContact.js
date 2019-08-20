b9101aefcae7fa2b453583701a1a00aaa67c8866
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-21 01:58:14 +0200

    build a form to create new contact

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput'
 
 class CreateContact extends React.Component {
     render() {
         return(
-            
-            <div>Create Contact!</div>
+
+            <div>
+                <Link className='close-create-contact' to='/'>Close</Link>
+                <form className='create-contact-form'>
+                <ImageInput
+                    className='create-contact-avatar-input'
+                    name='avatarURL'
+                    maxHeight={64}
+                />
+                <div className='create-contact-details'>
+                    <input type='text' name='name' placeholder='Name'/>
+                    <input type='text' name='email' placeholder='Email'/>
+                    <button>Add Contact</button>
+                </div>
+                </form>
+            </div>
         ) 
     }
 }
