7bc9002033fa7fbad8e300f42d652381bb70b38d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-21 02:15:05 +0200

    save new contact to the server

--- src/App.js
+++ src/App.js
@@ -22,6 +22,13 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
 
   render() {
     return (
@@ -32,7 +39,14 @@ class App extends Component {
             contacts={this.state.contacts}
           />
           )}/>
-          <Route path='/create' component={CreateContact}/>
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+          onCreateContact={(contact) => {
+            this.createContact(contact)
+            history.push('/')
+          }}
+          />
+        )}/>
       </div>
     )
   }
